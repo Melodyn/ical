cc53640b6505068cceb47e89594784d9db5b608c
Melodyn <samelodyn@gmail.com>
2020-11-07 20:32:54 +0300

    added heroku ci

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+import fastify from 'fastify';
 import app from '../index.js';
 
-console.log(app(process.argv[2]));
+const PORT = process.env.PORT || 5000;
+
+fastify()
+  .get('/', (req, res) => {
+    const { name } = req.query;
+    res.code(200).send(app(name));
+  })
+  .listen(8080, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
