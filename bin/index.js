5c72588eb75cc51ec44e91c85cc0156b7ec54ba6
Melodyn <samelodyn@gmail.com>
2020-12-19 12:17:01 +0300

    added logs

--- bin/index.js
+++ bin/index.js
@@ -8,17 +8,16 @@ const vkbridge = bridge.default;
 
 const PORT = process.env.PORT || 5000;
 
-vkbridge.send('VKWebAppInit', {})
-  .then(() => {
-    vkbridge.subscribe((e) => console.log(e));
-  })
-  .catch(console.error);
-
 express()
   .get('/', (req, res) => {
-    console.log(req);
+    console.log('vk_app_id', req.query.vk_app_id);
     res.send(app());
   })
   .listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
+    vkbridge.send('VKWebAppInit', {})
+      .then(() => {
+        vkbridge.subscribe((e) => console.log('VKWebAppInit', e));
+      })
+      .catch(console.error);
   });
