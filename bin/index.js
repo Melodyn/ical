ccc7708c11644f3c98ffdfaa5b4346db5501ef11
Melodyn <samelodyn@gmail.com>
2020-12-19 12:06:05 +0300

    added bridge

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
 import express from 'express';
+import bridge from '@vkontakte/vk-bridge';
 import app from '../index.js';
+
+const vkbridge = bridge.default;
 
 const PORT = process.env.PORT || 5000;
 
+vkbridge.send('VKWebAppInit', {})
+  .then(() => {
+    vkbridge.subscribe((e) => console.log(e));
+  })
+  .catch(console.error);
+
 express()
   .get('/', (req, res) => {
+    console.log(req);
     res.send(app());
   })
   .listen(PORT, '0.0.0.0', () => {
