44f099d80f69e10678dd432f922e0b8003340785
Melodyn <samelodyn@gmail.com>
2021-02-11 06:18:56 +0300

    remove calendar

--- __tests__/crud.test.js
+++ __tests__/crud.test.js
@@ -140,7 +140,7 @@ describe('Positive cases', () => {
     });
 
     expect(statusCode).toEqual(constants.HTTP_STATUS_OK);
-    expect(payload).toMatch(/iframe/gim);
+    expect(payload).toMatch(/Календарь/gim); // TODO вернуть iframe
   });
 
   // заглушено из-за отсутствия редиректов в мобильном приложении
