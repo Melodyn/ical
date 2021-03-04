2eab0dbc5090401b1867f2e34b0ee7a8c8ca3f14
Melodyn <samelodyn@gmail.com>
2021-03-04 19:29:33 +0300

    refactoring vkservice

--- libs/vk/widgetUpdater/index.js
+++ libs/vk/widgetUpdater/index.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import configValidator from '../../../utils/configValidator.cjs';
+import errors from '../../../utils/errors.cjs';
+
+const { ICalAppError } = errors;
 
 const { envsMap } = configValidator;
 
-const prodService = (apiURL) => ({ params }) => axios.get(apiURL, { params });
+const prodService = (apiURL) => ({ params }) => axios
+  .get(apiURL, { params })
+  .then(({ data }) => data)
+  .then((data) => {
+    if (!data.error) return data;
+
+    throw new ICalAppError(JSON.stringify(data.error));
+  });
+
 const devService = ({ widget }) => Promise.resolve(widget).then((res) => {
   console.log('widgetUpdater', JSON.stringify(res));
   return res;
 });
+
 const testService = ({ widget }) => Promise.resolve(widget);
 
 export default (env, apiURL) => {
