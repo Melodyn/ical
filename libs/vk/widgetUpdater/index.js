f37127fb12159a74d48dda08b3d504548f43134a
Melodyn <samelodyn@gmail.com>
2021-03-09 15:17:06 +0300

    hotfix widget updater

--- libs/vk/widgetUpdater/index.js
+++ libs/vk/widgetUpdater/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import FormData from 'form-data';
 import configValidator from '../../../utils/configValidator.cjs';
 import errors from '../../../utils/errors.cjs';
 
@@ -6,17 +7,23 @@ const { ICalAppError } = errors;
 
 const { envsMap } = configValidator;
 
-const prodService = (apiURL) => ({ params }) => axios({
-  method: 'POST',
-  url: apiURL,
-  params,
-})
-  .then(({ data }) => data)
-  .then((data) => {
-    if (!data.error) return data;
+const prodService = (apiURL) => ({ params }) => {
+  const body = new FormData();
+  Object.entries(params).forEach(([key, value]) => body.append(key, value));
 
-    throw new ICalAppError(JSON.stringify(data.error));
-  });
+  return axios({
+    method: 'POST',
+    url: apiURL,
+    data: body,
+    headers: body.getHeaders(),
+  })
+    .then(({ data }) => data)
+    .then((data) => {
+      if (!data.error) return data;
+
+      throw new ICalAppError(JSON.stringify(data.error));
+    });
+};
 
 const testService = async ({ widget }) => widget;
 
