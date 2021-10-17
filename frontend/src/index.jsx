cf8b1437e283e2286595d26a7f35ce3cfa5e16d0
Melodyn <samelodyn@gmail.com>
2021-10-17 09:30:04 +0300

    #199 added bridge

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@vkontakte/vkui/dist/vkui.css';
+import bridge from '@vkontakte/vk-bridge';
 import App from './App.jsx';
+
+bridge.send('VKWebAppInit');
 
 ReactDOM.render(<App />, document.getElementById('root'));
