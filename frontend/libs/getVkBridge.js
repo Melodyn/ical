c598f15df0e7a98801ff477c4f8fe936f2819589
Sergei Melodyn <samelodyn@gmail.com>
2022-01-21 17:28:59 +0300

    fix bridge

--- frontend/libs/getVkBridge.js
+++ frontend/libs/getVkBridge.js
@@ -3,7 +3,7 @@ import bridge from '@vkontakte/vk-bridge';
 const proxyBridge = new Proxy({}, { get: () => () => {} });
 
 const getVkBridge = ({ IS_PROD_ENV }) => (IS_PROD_ENV
-  ? bridge.default
+  ? bridge
   : proxyBridge);
 
 export default getVkBridge;
