9650098c8ace28604b9b5f9d8edde1f06a1fa940
Sergei Melodyn <samelodyn@gmail.com>
2022-01-21 14:31:16 +0000

    Release

--- frontend/libs/getVkBridge.js
+++
@@ -1,9 +0,0 @@
-import bridge from '@vkontakte/vk-bridge';
-
-const proxyBridge = new Proxy({}, { get: () => () => {} });
-
-const getVkBridge = ({ IS_PROD_ENV }) => (IS_PROD_ENV
-  ? bridge
-  : proxyBridge);
-
-export default getVkBridge;
